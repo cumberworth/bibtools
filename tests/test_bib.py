adbd4997cbb40f51290eeabda221c93fea0c1ca6
Alexander <alexandercumberworth@gmail.com>
2020-09-22 16:46:44 +0200

    Updated to current version of pytest.
    Tests failing because bibfile no longer takes a filename.

--- tests/test_bib.py
+++ tests/test_bib.py
@@ -2,24 +2,36 @@
 
 import pytest
 
-import bibtools as btl
+import bibtools.bib as btl
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_simple')
+def search_string_simple_fixture():
+    return search_string_simple()
+
+
 def search_string_simple():
     input_list = ['field:keywords', 'stuff', 'thing']
     search_string = btl.SearchString(input_list)
     return search_string
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_negation')
+def search_string_negation_fixture():
+    return search_string_negation()
+
+
 def search_string_negation():
     input_list = ['not', 'field:keywords', 'stuff', 'thing']
     search_string = btl.SearchString(input_list)
     return search_string
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_conjunction')
+def search_string_conjunction_fixture():
+    return search_string_conjunction()
+
+
 def search_string_conjunction():
     input_list = ['field:keywords', 'stuff',
                   'thing', 'and', 'field:author', 'pande']
@@ -27,7 +39,11 @@ def search_string_conjunction():
     return search_string
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_disjunction')
+def search_string_disjunction_fixture():
+    return search_string_disjunction()
+
+
 def search_string_disjunction():
     input_list = ['field:keywords', 'stuff',
                   'thing', 'or', 'field:author', 'pande']
@@ -40,12 +56,14 @@ class TestSearchString:
     @pytest.mark.parametrize('search_string, field_list, terms_list', [
         (search_string_simple(), ['keywords'], [['stuff', 'thing']]),
         (search_string_negation(), ['keywords'], [['stuff', 'thing']]),
-        (search_string_conjunction(), ['keywords', 'author'], [['stuff',
-                                                                'thing'],
-                                                               ['pande']]),
-        (search_string_disjunction(), ['keywords', 'author'], [['stuff',
-                                                                'thing'],
-                                                               ['pande']])])
+        (search_string_conjunction(), ['keywords', 'author'],
+            [['stuff',
+              'thing'],
+             ['pande']]),
+        (search_string_disjunction(), ['keywords', 'author'],
+            [['stuff',
+              'thing'],
+             ['pande']])])
     def test__iter__(self, search_string, field_list, terms_list):
         test_field_list = []
         test_terms_list = []
@@ -74,27 +92,43 @@ class TestSearchString:
         assert test_match == match
 
 
-@pytest.fixture()
+@pytest.fixture(name='bibfile_pande')
+def bibfile_pande_fixture():
+    return bibfile_pande()
+
+
 def bibfile_pande():
     bibfile_pande = btl.BibFile('pande2010.bib')
     return bibfile_pande
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_pande_1')
+def search_string_pande_1_fixture():
+    return search_string_pande_1()
+
+
 def search_string_pande_1():
     input_list = ['field:keywords', 'review', 'msm']
     search_string = btl.SearchString(input_list)
     return search_string
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_pande_2')
+def search_string_pande_2_fixture():
+    return search_string_pande_2()
+
+
 def search_string_pande_2():
     input_list = ['field:keywords', 'review', 'msm', 'thing']
     search_string = btl.SearchString(input_list)
     return search_string
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_pande_3')
+def search_string_pande_3_fixture():
+    return search_string_pande_3()
+
+
 def search_string_pande_3():
     input_list = ['field:keywords', 'review',
                   'msm', 'and', 'field:author', 'pande']
@@ -102,7 +136,11 @@ def search_string_pande_3():
     return search_string
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_pande_4')
+def search_string_pande_4_fixture():
+    return search_string_pande_4()
+
+
 def search_string_pande_4():
     input_list = ['field:keywords', 'review', 'msm',
                   'and', 'not', 'field:author', 'pande']
@@ -110,7 +148,11 @@ def search_string_pande_4():
     return search_string
 
 
-@pytest.fixture
+@pytest.fixture(name='search_string_pande_5')
+def search_string_pande_5_fixture():
+    return search_string_pande_5()
+
+
 def search_string_pande_5():
     input_list = ['field:keywords', 'review',
                   'msm', 'or', 'not', 'field:author', 'pande']
